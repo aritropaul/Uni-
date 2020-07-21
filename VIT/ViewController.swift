c4e1928522d4d6e7ac2f4bb427e0ca27f9354bc2
Aritro Paul <aritro.paul61@gmail.com>
2020-07-21 16:02:08 +0530

    Initial Commit

---
+++ VIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIT
+//
+//  Created by Aritro Paul on 21/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
